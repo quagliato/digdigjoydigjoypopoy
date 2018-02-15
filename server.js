9bdf09978ca1fdb32a6f2a58727b9c28a98efaa5
Eduardo Quagliato <eduardo@quagliato.me>
2018-02-15 20:23:20 -0200

    :hammer: every code should be upgraded and refactored, right?

--- server.js
+++ server.js
@@ -3,149 +3,170 @@
 // Curitiba, Brazil
 // Author: Eduardo Quagliato <eduardo@quagliato.me>
 
-var bodyParser = require("body-parser");
-var express = require("express");
+const bodyParser = require('body-parser')
+const dns = require('native-dns')
+const express = require('express')
+const fs = require('fs')
+const unirest = require('unirest')
 
 // Get yours in http://jsonwhois.com
-var JSON_WHOIS_KEY = process.env.JSON_WHOIS_KEY || null;
+const JSON_WHOIS_KEY = process.env.JSON_WHOIS_KEY || null
 
-app = express();
+app = express()
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: true
-}));
+}))
 
 /****************************************************************************/
 /* HEALTH CHECK */
 /****************************************************************************/
-app.get("/status", function(request, response, body){
-  response.set('Access-Control-Allow-Origin', '*');
-  response.set('Content-Type', 'text/json');
-  response.status(200).send(JSON.stringify({"status":"OK", "method":"POST"}));
-});
-
-app.post("/status", function(request, response, body){
-  response.set('Access-Control-Allow-Origin', '*');
-  response.set('Content-Type', 'text/json');
-  response.status(200).send(JSON.stringify({"status":"OK", "method":"GET"}));
-});
+app.all('/status', (request, response, body) => {
+  response.set('Access-Control-Allow-Origin', '*')
+  response.set('Content-Type', 'text/json')
+  response.status(200).send(JSON.stringify({
+    status: 'OK',
+    method: 'POST'
+  }))
+})
 
 /***************************************************************************/
 /* GET PAGE */
 /***************************************************************************/
+app.get('/', (request, response, body) => {
+  response.set('Access-Control-Allow-Origin', '*')
+  response.set('Content-Type', 'text/html')
 
-app.get('/', function(request, response, body) {
-  response.set('Access-Control-Allow-Origin', '*');
-  response.set('Content-Type', 'text/html');
-
-  var fs = require('fs');
-  fs.readFile('README.html', 'utf8', function(err, data) {
+  fs.readFile('README.html', 'utf8', (err, data) => {
     if (err) {
-      console.log(err);
-      response.status(500).end(JSON.stringify({"status":"ERROR","description":"Couldn't process your request."}));
-    } else {
-      response.end(data);
+      console.log(err)
+      response.status(500).end(JSON.stringify({
+        status: 'ERROR',
+        description: 'Couldn\'t process your request.'
+      }))
+      return
     }
-  });
-});
+    
+    response.end(data)
+  })
+})
 
 /**************************************************************************/
 /* DIG */
 /**************************************************************************/
 
-app.post('/', function(request, response, body) {
-  response.set('Access-Control-Allow-Origin', '*');
-  response.set('Content-Type', 'text/json');
+app.get('/dig', (request, response, body) => {
+  response.set('Access-Control-Allow-Origin', '*')
+  response.set('Content-Type', 'text/json')
 
-  var params = Object.keys(request.body)[0];
-  params = JSON.parse(params);
-  if (!params.hasOwnProperty('domain') || !params.hasOwnProperty('type')) {
-    response.status(404).end(JSON.stringify({"status":"ERROR", "description":"Domain and Type are mandatory parameters."}));
-  } else {
-  var dns = require("native-dns");
+  if (!request.query.domain || !request.query.type) {
+    response.status(404).end(JSON.stringify({
+      status: 'ERROR',
+      description: 'Domain and Type are mandatory parameters.'
+    }))
+
+    return
+  }
+
   dns.Request({
     question: dns.Question({
-      name: params.domain,
-      type: params.type
+      name: request.query.domain,
+      type: request.query.type
     }),
-    server: {"address":"8.8.8.8","port":"53","type":"udp"},
+    server: {
+      address: '8.8.8.8',
+      port: 53,
+      type: 'udp'
+    },
     timeout: 2000
-  }).on('timeout', function() {
-    response.status(500).end(JSON.stringify({"status":"ERROR", "description":"Timeout in fetching DNS info."}));
-  }).on('message', function(err, res) {
-    if (err !== null && err !== undefined) {
-      response.status(500).end(JSON.stringify({"status":"ERROR", "description":"Couldn't process your request."}));
-    } else {
-
-    var records = [];
-    for(var i = 0; i < res.answer.length; i++) {
-      var record = res.answer[i];
-      var newRecord = {
-        "name" : record.name,
-        "type" : params.type,
-        "ttl"  : record.ttl
-      };
-      
-      if (params.type.toUpperCase() === "MX") newRecord["priority"] = record.priority;
-      
-      switch (params.type.toUpperCase()) {
-        case "MX":
-          newRecord["address"] = record.exchange;
-          break;
-        case "A":
-          newRecord["address"] = record.address;
-          break;
-        case "TXT":
-        case "NS":
-          newRecord["address"] = record.data;
-          break;
-      }
-      
-      records[records.length] = newRecord;
-      }
+  }).on('timeout', () => {
+    return response.status(500).end(JSON.stringify({
+      status: 'ERROR',
+      description: 'Timeout in fetching DNS info.'
+    }))
+  }).on('message', (err, res) => {
+    if (err) {
+      return response.status(500).end(JSON.stringify({
+        status: 'ERROR',
+        description: 'Couldn\'t process your request.'
+      }))
     }
 
-    response.status(200).end(JSON.stringify({"status":"OK", "records":records}));
-  }).send();
-  }
-});
+    const records = []
+    for(let i = 0; i < res.answer.length; i++) {
+      const record = res.answer[i]
+      const newRecord = {
+        "name" : record.name,
+        "type" : request.query.type,
+        "ttl"  : record.ttl
+      }
+      
+      if (request.query.type.toUpperCase() === "MX") newRecord["priority"] = record.priority
+      
+      switch (request.query.type.toUpperCase()) {
+        case "MX":
+          newRecord["address"] = record.exchange
+          break
+        case "A":
+          newRecord["address"] = record.address
+          break
+        case "TXT":
+        case "NS":
+          newRecord["address"] = record.data
+          break
+      }
+      
+      records.push(newRecord);
+    }
+
+    response.status(200).end(JSON.stringify({
+      status: 'OK',
+      records: records
+    }));
+  }).send()
+})
 
 /*****************************************************************************/
 /* WHOIS */
 /*****************************************************************************/
 
-app.post("/whois", function(request, response, body){
-  response.set('Access-Control-Allow-Origin', '*');
-  response.set('Content-Type', 'text/json');
+app.get('/whois', (request, response, body) => {
+  response.set('Access-Control-Allow-Origin', '*')
+  response.set('Content-Type', 'text/json')
 
-  var params = Object.keys(request.body)[0];
-  params = JSON.parse(params);
-  if (!params.hasOwnProperty('domain')) {
-    response.status(404).end(JSON.stringify({"status":"ERROR", "description":"Domain is a mandatory parameter."}));
+  if (!request.query.domain) {
+    response.status(404).end(JSON.stringify({ 
+      status: 'ERROR',
+      description: 'Domain is a mandatory parameter.'
+    }));
+
+    return
   }
 
   if (JSON_WHOIS_KEY === null) {
-    console.log("No JSON_WHOIS_KEY setted in the environment.");
-    process.exit(1);
+    console.log('No JSON_WHOIS_KEY setted in the environment.')
+    process.exit(1)
   }
-
-  var unirest = require("unirest");
 
   unirest.get('https://jsonwhois.com/api/v1/whois')
    .headers({
      'Accept': 'application/json',
-     'Authorization': 'Token token=' + JSON_WHOIS_KEY
+     'Authorization': `Token token=${JSON_WHOIS_KEY}`
    })
    .query({
-     "domain": params.domain
+     'domain': request.query.domain
    })
-   .end(function(jsonWhoisResponse){
-     console.log(jsonWhoisResponse.body);
-     response.status(200).end(JSON.stringify({"status":"OK", "records":jsonWhoisResponse.body.nameservers}));
-   });
-});
+   .end((jsonWhoisResponse) => {
+     console.log(jsonWhoisResponse.body)
+     response.status(200).end(JSON.stringify({
+       status: 'OK',
+       records: jsonWhoisResponse.body.nameservers
+     }))
+   })
+})
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-app.listen(port);
+app.listen(port)
+
